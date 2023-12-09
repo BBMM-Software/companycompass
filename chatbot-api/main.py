701983d78fed96a10b50e21467e56f11512dfc85
Stamatie Mihnea <stamatiemihnea@gmail.com>
2023-12-09 22:03:40 +0200

    Ask service added

--- chatbot-api/main.py
+++ chatbot-api/main.py
@@ -2,6 +2,8 @@ from flask import Flask, request
 
 app = Flask("chatbot-api")
 
+askServiceMap = {}
+
 @app.route('/scrape', methods=['POST']) # company_site => generate csv 
 def scrape():
   if request.method == 'POST':
@@ -9,6 +11,13 @@ def scrape():
   
 @app.route('/ask') # question => response
 def ask():
-  return 'Hello from Server'
+  company_name = request.args.get("company_name")
+  company_site = request.args.get("company_site")
+  question = request.args.get("question")
+  if company_site not in askServiceMap:
+    askServiceMap[company_site] = AskService(company_name, company_site, client)
+  
+  askService = askServiceMap[company_site]
+  return askService.ask_question(question)
 
 app.run(port=1280, debug=True)
