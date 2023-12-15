603b47c30097e806ac19b0a91b112921ba6f286c
Edi Bicu <bicuedici@gmail.com>
2023-12-15 08:57:23 +0200

    avoid forbidden

--- chatbot-api/crawl_service.py
+++ chatbot-api/crawl_service.py
@@ -33,8 +33,25 @@ def get_hyperlinks(url):
     
     # Try to open the URL and read the HTML
     try:
+        req = urllib.request.Request(url)
+        headers = {
+            "authority": "httpbin.org",
+            "cache-control": "max-age=0",
+            "sec-ch-ua": '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
+            "sec-ch-ua-mobile": "?0",
+            "upgrade-insecure-requests": "1",
+            "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36",
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "sec-fetch-site": "none",
+            "sec-fetch-mode": "navigate",
+            "sec-fetch-user": "?1",
+            "sec-fetch-dest": "document",
+            "accept-language": "en-US,en;q=0.9",
+        }
+        for k1, k2 in headers.items(): 
+            req.add_header(k1, k2)
         # Open the URL and read the HTML
-        with urllib.request.urlopen(url) as response:
+        with urllib.request.urlopen(req) as response:
 
             # If the response is not HTML, return an empty list
             if not response.info().get('Content-Type').startswith("text/html"):
