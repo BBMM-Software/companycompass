6fc2c678cfa42534871848f21bfc7c22ff706a45
Adrian <adrian212005@yahoo.ro>
2024-03-02 22:10:22 +0200

    removed bbmm-portal

--- bbmm-portal/components/ui/logo.tsx
+++
@@ -1,9 +0,0 @@
-import Link from 'next/link'
-
-export default function Logo() {
-  return (
-    <Link href="/" className="block" aria-label="Cruip">
-      <img src="/images/companycompasslogo.jpg" height="50" width="50"></img>
-    </Link>
-  )
-}
